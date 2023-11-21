5dd3c58eadc24b9e501440131c7d9a994f28c066
Y0ungRebel <ilja.krivencko@yandex.ru>
2023-11-21 16:52:37 +0300

    2nd commit (21/11/23)

--- app/src/main/java/com/example/smarthouse/MainActivity.kt
+++ app/src/main/java/com/example/smarthouse/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.smarthouse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var btn_registration: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //btn_registration = findViewById(R.id.btn_registration);
+    }
+    fun onClick (view:View){
+        val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
+        startActivity(intent)
     }
 }
