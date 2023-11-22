48abaf8630aacece4e563e4ffaa574de1702c9b0
Y0ungRebel <ilja.krivencko@yandex.ru>
2023-11-22 22:46:34 +0300

    3rd commit (22/11/23)

--- app/src/main/java/com/example/smarthouse/CreatePinActivity.kt
+++ app/src/main/java/com/example/smarthouse/CreatePinActivity.kt
@@ -1,11 +1,17 @@
 package com.example.smarthouse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class CreatePinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_pin)
+    }
+    fun onClick (view:View){
+        val intent = Intent(this@CreatePinActivity, PinActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/smarthouse/RegistrationActivity.kt
+++ app/src/main/java/com/example/smarthouse/RegistrationActivity.kt
@@ -1,11 +1,18 @@
 package com.example.smarthouse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+    }
+
+    fun onClick (view: View){
+        val intent = Intent(this@RegistrationActivity, CreatePinActivity::class.java)
+        startActivity(intent)
     }
 }
