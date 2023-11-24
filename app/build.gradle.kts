68956b88065f4eef96e562000ae2edb9f58784da
Y0ungRebel <ilja.krivencko@yandex.ru>
2023-11-24 23:25:00 +0300

    5th commit (24/11/23)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("io.ktor:ktor-client-cio:2.3.4")
+    implementation(platform("io.github.jan-tennert.supabase:bom:1.4.1"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:realtime-kt")
 }
--- app/src/main/java/com/example/smarthouse/RegistrationActivity.kt
+++ app/src/main/java/com/example/smarthouse/RegistrationActivity.kt
@@ -4,15 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var editEmail: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        editEmail = findViewById(R.id.mail)
     }
 
     fun onClick (view: View){
-        val intent = Intent(this@RegistrationActivity, CreatePinActivity::class.java)
-        startActivity(intent)
+        //val intent = Intent(this@RegistrationActivity, CreatePinActivity::class.java)
+        //startActivity(intent)
+
+        val emailValue = editEmail.getText().toString()
+        val emailPattern = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" + ")+"
+
+        //preferences = getSharedPreferences("UserInfo", 0)
+
+        if (emailValue.matches(emailPattern.toRegex())) {
+            //val editor: SharedPreferences.Editor = preferences.edit()
+            //editor.putString("email", emailValue)
+            //editor.apply()
+            Toast.makeText(this, "Успешно!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, CreatePinActivity::class.java)
+            startActivity(intent)
+        } else if (!emailValue.matches(emailPattern.toRegex())) {
+            Toast.makeText(this, "Почта введена неверно или не введена вовсе!", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 }
